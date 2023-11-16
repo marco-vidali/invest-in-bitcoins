46d2b233fc1a1d81c24e9b75c6b88077ad16e97a
marco-vidali <marco-vidali-4@proton.me>
2023-11-16 10:57:16 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+
+const INITIAL_BITCOINS = 10;
+
+const initialState = {
+  bitcoins: Number(localStorage.getItem("bitcoins")) || INITIAL_BITCOINS,
+  investing: false,
+  amount: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "startInvesting":
+      return {
+        ...state,
+        investing: true,
+      };
+    case "invest":
+      if (state.bitcoins >= state.amount) {
+        return {
+          ...state,
+          investing: false,
+          amount: null,
+          bitcoins: state.bitcoins + (Math.random() * 2 - 1) * state.amount,
+        };
+      } else {
+        return state;
+      }
+    case "changeAmount":
+      return {
+        ...state,
+        amount: Number(action.payload),
+      };
+    case "restart":
+      return { ...state, bitcoins: INITIAL_BITCOINS };
+    default:
+      throw new Error("Action type not recognized");
+  }
+}
 
 function App() {
+  const [{ bitcoins, investing, amount }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(
+    function () {
+      localStorage.setItem("bitcoins", bitcoins);
+    },
+    [bitcoins]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hi Bitcoiner!</h1>
+      <div>
+        <hr />
+        <p>Number of Bitcoins: ₿{bitcoins.toFixed(2)}</p>
+        <hr />
+      </div>
+      <div>
+        {investing ? (
+          <>
+            <input
+              type="number"
+              placeholder="Bitcoins to invest"
+              value={amount}
+              onChange={(e) =>
+                dispatch({ type: "changeAmount", payload: e.target.value })
+              }
+            />
+            &nbsp;
+            <button onClick={() => dispatch({ type: "invest" })}>Invest</button>
+          </>
+        ) : (
+          <button
+            disabled={investing}
+            onClick={() => dispatch({ type: "startInvesting" })}
+          >
+            Invest in Bitcoin
+          </button>
+        )}
+      </div>
+      <br />
+      <button onClick={() => dispatch({ type: "restart" })}>Restart</button>
+    </>
   );
 }
 
